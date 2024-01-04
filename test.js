c298eb95b40beb3eee449fff2aa1eec069606433
flof <155272834+inspflof@users.noreply.github.com>
2024-01-04 03:04:39 +0000

    modified:   test.js

--- test.js
+++ test.js
@@ -1,3 +1,23 @@
-// bon la je t'avoue que ça me fais un peu suer
-// mais j'aime bien
-// et pis ça me servira bien un jour
+let a = 0
+let c=0
+
+function getRandomInt(max) {
+    a = Math.floor(Math.random() * max);
+  }  
+  console.log(getRandomInt(1000));
+
+b = prompt(`Devinez le nombre`)
+
+while (c == 0){
+    if (b>a){
+        b=prompt(`c'est moins`)
+    }
+    else if (b<a){
+        b=prompt(`c'est plus`)
+    }
+    else{
+        c=1
+    }
+}
+
+console.log(`Félicitation, vous avez gagné. Le nombre était : `, a)
