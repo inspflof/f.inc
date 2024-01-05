03e277d41c600f7e517062d082f8dab5e00645e0
flof <155272834+inspflof@users.noreply.github.com>
2024-01-05 02:21:33 +0000

    modified:   index.html 	modified:   style.css 	new file:   test.css 	new file:   test.html 	modified:   test.js

--- test.js
+++ test.js
@@ -1,25 +1,53 @@
-let a = 0
-let c=0
+// let a = 0
+// let c=0
 
-function getRandomInt(max) {
-    a = Math.floor(Math.random() * max);
-  }  
-  console.log(getRandomInt(1000));
+// function getRandomInt(max) {
+//     a = Math.floor(Math.random() * max);
+//   }  
+//   console.log(getRandomInt(1000));
 
-b = prompt(`Devinez le nombre`)
+// b = prompt(`Devinez le nombre`)
 
-while (c == 0){
-    if (b>a){
-        b=prompt(`c'est moins`)
-    }
-    else if (b<a){
-        b=prompt(`c'est plus`)
-    }
-    else{
-        c=1
+// while (c == 0){
+//     if (b>a){
+//         b=prompt(`c'est moins`)
+//     }
+//     else if (b<a){
+//         b=prompt(`c'est plus`)
+//     }
+//     else if (b==a){
+//         c=1
+//         console.log(`Félicitation, vous avez gagné. Le nombre était : `, a)
+//     }
+//     else if (b !== a){
+//         c=1
+//         console.log(`Le caractere `,b,` n'est pas valide`)
+//     }
+// }
+// ----------------------------------------------------------
+
+// function candrive (age, pays) {
+//     if (
+//         (age >= 18 && pays === `FR`) ||
+//         (age >= 16 && pays === `US`)
+//     ) {
+//         return true
+//     }
+//     return false
+// }
+
+// console.log(candrive(19, `FR`))
+
+const a = {
+    firstname: `John`,
+    lastname:`Doe`,
+    fullname: function (){
+        console.log(this.firstname, this.lastname)
     }
 }
 
-console.log(`Félicitation, vous avez gagné. Le nombre était : `, a)
+const mafonction = (p1, p2) =>{
+    console.log(p1, p2)
+}
 
-// hello world !
+mafonction(1,2)
